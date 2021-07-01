f46fb7e5dccee654dc1d4eb4e4c39d2e1d74e967
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-07-01 20:05:20 -0300

    Review of the migration code

--- migrations/02_deploy_contracts.js
+++ migrations/02_deploy_contracts.js
@@ -47,7 +47,6 @@ const NOW = Math.floor(Date.now() / 1000);
 
 const CHAIN_ID = 56; // bscmain
 const GRO = '0x336eD56D8615271b38EcEE6F4786B55d0EE91b96'; // bscmain (needs mint() move() burn())
-const PAUSE_DELAY = '0';
 const DS_AUTHORITY = '0x0000000000000000000000000000000000000000'; // review this
 const ESM_MIN = '50000000000000000000000'; // review this
 const ETH = '0x2170Ed0880ac9A755fd29B2688956BD959F933F8'; // bscmain
@@ -63,139 +62,206 @@ const LERP_DURATION = 7 * 24 * 60 * 60;
 module.exports = async (deployer, network, [account]) => {
   const web3 = DssDeploy.interfaceAdapter.web3;
 
+  // deploys DSProxyFactory
   console.log('Publishing Proxy Factory...');
   await deployer.deploy(DSProxyFactory);
   const dsProxyFactory = await DSProxyFactory.deployed();
+  const PROXY_FACTORY = dsProxyFactory.address;
+  console.log('PROXY_FACTORY=' + PROXY_FACTORY);
 
+  // deploys ProxyRegistry
   console.log('Publishing Proxy Registry...');
-  await deployer.deploy(ProxyRegistry, dsProxyFactory.address);
+  await deployer.deploy(ProxyRegistry, PROXY_FACTORY);
   const proxyRegistry = await ProxyRegistry.deployed();
+  const PROXY_REGISTRY = proxyRegistry.address;
+  console.log('PROXY_REGISTRY=' + PROXY_REGISTRY);
 
   // deploys VatFab
-  console.log('Publishing VatFab contract...');
+  console.log('Publishing VatFab...');
   await deployer.deploy(VatFab);
   const vatFab = await VatFab.deployed();
+  const VAT_FAB = vatFab.address;
+  console.log('VAT_FAB=' + VAT_FAB);
 
   // deploys JugFab
-  console.log('Publishing JugFab contract...');
+  console.log('Publishing JugFab...');
   await deployer.deploy(JugFab);
   const jugFab = await JugFab.deployed();
+  const JUG_FAB = jugFab.address;
+  console.log('JUG_FAB=' + JUG_FAB);
 
   // deploys VowFab
-  console.log('Publishing VowFab contract...');
+  console.log('Publishing VowFab...');
   await deployer.deploy(VowFab);
   const vowFab = await VowFab.deployed();
+  const VOW_FAB = vowFab.address;
+  console.log('VOW_FAB=' + VOW_FAB);
 
   // deploys CatFab
-  console.log('Publishing CatFab contract...');
+  console.log('Publishing CatFab...');
   await deployer.deploy(CatFab);
   const catFab = await CatFab.deployed();
+  const CAT_FAB = catFab.address;
+  console.log('CAT_FAB=' + CAT_FAB);
 
   // deploys DogFab
-  console.log('Publishing DogFab contract...');
+  console.log('Publishing DogFab...');
   await deployer.deploy(DogFab);
   const dogFab = await DogFab.deployed();
+  const DOG_FAB = dogFab.address;
+  console.log('DOG_FAB=' + DOG_FAB);
 
   // deploys DaiFab
-  console.log('Publishing DaiFab contract...');
+  console.log('Publishing DaiFab...');
   await deployer.deploy(DaiFab);
   const daiFab = await DaiFab.deployed();
+  const DAI_FAB = daiFab.address;
+  console.log('DAI_FAB=' + DAI_FAB);
 
   // deploys DaiJoinFab
-  console.log('Publishing DaiJoinFab contract...');
+  console.log('Publishing DaiJoinFab...');
   await deployer.deploy(DaiJoinFab);
   const daiJoinFab = await DaiJoinFab.deployed();
+  const MCD_JOIN_FAB = daiJoinFab.address;
+  console.log('MCD_JOIN_FAB=' + MCD_JOIN_FAB);
 
   // deploys FlapFab
-  console.log('Publishing FlapFab contract...');
+  console.log('Publishing FlapFab...');
   await deployer.deploy(FlapFab);
   const flapFab = await FlapFab.deployed();
+  const FLAP_FAB = flapFab.address;
+  console.log('FLAP_FAB=' + FLAP_FAB);
 
   // deploys FlopFab
-  console.log('Publishing FlopFab contract...');
+  console.log('Publishing FlopFab...');
   await deployer.deploy(FlopFab);
   const flopFab = await FlopFab.deployed();
+  const FLOP_FAB = flopFab.address;
+  console.log('FLOP_FAB=' + FLOP_FAB);
 
   // deploys FlipFab
-  console.log('Publishing FlipFab contract...');
+  console.log('Publishing FlipFab...');
   await deployer.deploy(FlipFab);
   const flipFab = await FlipFab.deployed();
+  const FLIP_FAB = flipFab.address;
+  console.log('FLIP_FAB=' + FLIP_FAB);
 
   // deploys ClipFab
-  console.log('Publishing ClipFab contract...');
+  console.log('Publishing ClipFab...');
   await deployer.deploy(ClipFab);
   const clipFab = await ClipFab.deployed();
+  const CLIP_FAB = clipFab.address;
+  console.log('CLIP_FAB=' + CLIP_FAB);
 
   // deploys SpotFab
-  console.log('Publishing SpotFab contract...');
+  console.log('Publishing SpotFab...');
   await deployer.deploy(SpotFab);
   const spotFab = await SpotFab.deployed();
+  const SPOT_FAB = spotFab.address;
+  console.log('SPOT_FAB=' + SPOT_FAB);
 
   // deploys PotFab
-  console.log('Publishing PotFab contract...');
+  console.log('Publishing PotFab...');
   await deployer.deploy(PotFab);
   const potFab = await PotFab.deployed();
+  const POT_FAB = potFab.address;
+  console.log('POT_FAB=' + POT_FAB);
 
   // deploys EndFab
-  console.log('Publishing EndFab contract...');
+  console.log('Publishing EndFab...');
   await deployer.deploy(EndFab);
   const endFab = await EndFab.deployed();
+  const END_FAB = endFab.address;
+  console.log('END_FAB=' + END_FAB);
 
   // deploys ESMFab
-  console.log('Publishing ESMFab contract...');
+  console.log('Publishing ESMFab...');
   await deployer.deploy(ESMFab);
   const esmFab = await ESMFab.deployed();
+  const ESM_FAB = esmFab.address;
+  console.log('ESM_FAB=' + ESM_FAB);
 
   // deploys PauseFab
-  console.log('Publishing PauseFab contract...');
+  console.log('Publishing PauseFab...');
   await deployer.deploy(PauseFab);
   const pauseFab = await PauseFab.deployed();
+  const PAUSE_FAB = pauseFab.address;
+  console.log('PAUSE_FAB=' + PAUSE_FAB);
 
-  console.log('Publishing Gov Token contract...');
+  console.log('Publishing Gov Token...');
   await deployer.deploy(DSToken, "MKR");
-  const token = await DSToken.deployed();
-  const GOV = token.address;
+  const govToken = await DSToken.deployed();
+  const MCD_GOV = govToken.address;
+  console.log('MCD_GOV=' + MCD_GOV);
 
   // deploys DssDeploy
-  console.log('Publishing DssDeploy contract...');
+  console.log('Publishing DssDeploy...');
   await deployer.deploy(DssDeploy);
   const dssDeploy = await DssDeploy.deployed();
-
-  console.log('Adding fabs #1...');
-  await dssDeploy.addFabs1(vatFab.address, jugFab.address, vowFab.address, catFab.address, dogFab.address, daiFab.address, daiJoinFab.address);
-
-  console.log('Adding fabs #2...');
-  await dssDeploy.addFabs2(flapFab.address, flopFab.address, flipFab.address, clipFab.address, spotFab.address, potFab.address, endFab.address, esmFab.address, pauseFab.address);
+  await dssDeploy.addFabs1(VAT_FAB, JUG_FAB, VOW_FAB, CAT_FAB, DOG_FAB, DAI_FAB, MCD_JOIN_FAB);
+  await dssDeploy.addFabs2(FLAP_FAB, FLOP_FAB, FLIP_FAB, CLIP_FAB, SPOT_FAB, POT_FAB, END_FAB, ESM_FAB, PAUSE_FAB);
 
   console.log('Deploying DSRoles...');
   await deployer.deploy(DSRoles);
   const dsRoles = await DSRoles.deployed();
+  const MCD_ADM = dsRoles.address;
+  console.log('MCD_ADM=' + MCD_ADM);
   await dsRoles.setRootUser(account, true);
 
   console.log('Deploying Vat...');
   await dssDeploy.deployVat();
+  const MCD_VAT = await dssDeploy.vat();
+  console.log('MCD_VAT=' + MCD_VAT);
+  const MCD_SPOT = await dssDeploy.spotter();
+  console.log('MCD_SPOT=' + MCD_SPOT);
 
   console.log('Deploying Dai...');
   await dssDeploy.deployDai(CHAIN_ID);
+  const MCD_DAI = await dssDeploy.dai();
+  console.log('MCD_DAI=' + MCD_DAI);
+  const MCD_JOIN_DAI = await dssDeploy.daiJoin();
+  console.log('MCD_JOIN_DAI=' + MCD_JOIN_DAI);
 
   console.log('Deploying Taxation...');
   await dssDeploy.deployTaxation();
+  const MCD_JUG = await dssDeploy.jug();
+  console.log('MCD_JUG=' + MCD_JUG);
+  const MCD_POT = await dssDeploy.pot();
+  console.log('MCD_POT=' + MCD_POT);
 
   console.log('Deploying Auctions...');
-  await dssDeploy.deployAuctions(GOV);
+  await dssDeploy.deployAuctions(MCD_GOV);
+  const MCD_FLAP = await dssDeploy.flap();
+  console.log('MCD_FLAP=' + MCD_FLAP);
+  const MCD_FLOP = await dssDeploy.flop();
+  console.log('MCD_FLOP=' + MCD_FLOP);
+  const MCD_VOW = await dssDeploy.vow();
+  console.log('MCD_VOW=' + MCD_VOW);
 
   console.log('Deploying Liquidator...');
   await dssDeploy.deployLiquidator();
+  const MCD_CAT = await dssDeploy.cat();
+  console.log('MCD_CAT=' + MCD_CAT);
+  const MCD_DOG = await dssDeploy.dog();
+  console.log('MCD_DOG=' + MCD_DOG);
 
   console.log('Deploying End...');
   await dssDeploy.deployEnd();
+  const MCD_END = await dssDeploy.end();
+  console.log('MCD_END=' + MCD_END);
 
   console.log('Deploying Pause...');
-  await dssDeploy.deployPause(PAUSE_DELAY, DS_AUTHORITY);
-  const pause = await DSPause.at(await dssDeploy.pause());
+  await dssDeploy.deployPause(0, MCD_ADM);
+  const MCD_PAUSE = await dssDeploy.pause();
+  console.log('MCD_PAUSE=' + MCD_PAUSE);
+  const pause = await DSPause.at(MCD_PAUSE);
+  const MCD_PAUSE_PROXY = await pause.proxy();
+  console.log('MCD_PAUSE_PROXY=' + MCD_PAUSE_PROXY);
 
   console.log('Deploying ESM...');
-  await dssDeploy.deployESM(GOV, ESM_MIN);
+  await dssDeploy.deployESM(MCD_GOV, ESM_MIN);
+  const MCD_ESM = await dssDeploy.esm();
+  console.log('MCD_ESM=' + MCD_ESM);
 
   console.log('Deploying Collateral Flip #1...');
   {
@@ -206,15 +272,15 @@ module.exports = async (deployer, network, [account]) => {
     await median.setBar(MEDIAN_BAR);
     await median.lift(MEDIAN_ADDRESS_LIST);
     await median.kiss(osm.address);
-    await median.rely(await pause.proxy());
+    await median.rely(MCD_PAUSE_PROXY);
     await median.deny(await dssDeploy.owner());
-    await osm.kiss(await dssDeploy.end());
-    await osm.kiss(await dssDeploy.spotter());
-    await osm.rely(await pause.proxy());
+    await osm.kiss(MCD_END);
+    await osm.kiss(MCD_SPOT);
+    await osm.rely(MCD_PAUSE_PROXY);
     await osm.deny(await dssDeploy.owner());
-    await deployer.deploy(GemJoin, await dssDeploy.vat(), web3.utils.asciiToHex('ETH-A'), ETH);
+    await deployer.deploy(GemJoin, MCD_VAT, web3.utils.asciiToHex('ETH-A'), ETH);
     const gemJoin = await GemJoin.deployed();
-    await gemJoin.rely(await pause.proxy());
+    await gemJoin.rely(MCD_PAUSE_PROXY);
     await gemJoin.deny(await dssDeploy.owner());
     await dssDeploy.deployCollateralFlip(web3.utils.asciiToHex('ETH-A'), gemJoin.address, osm.address);
   }
@@ -228,15 +294,15 @@ module.exports = async (deployer, network, [account]) => {
     await median.setBar(MEDIAN_BAR);
     await median.lift(MEDIAN_ADDRESS_LIST);
     await median.kiss(osm.address);
-    await median.rely(await pause.proxy());
+    await median.rely(MCD_PAUSE_PROXY);
     await median.deny(await dssDeploy.owner());
-    await osm.kiss(await dssDeploy.end());
-    await osm.kiss(await dssDeploy.spotter());
-    await osm.rely(await pause.proxy());
+    await osm.kiss(MCD_END);
+    await osm.kiss(MCD_SPOT);
+    await osm.rely(MCD_PAUSE_PROXY);
     await osm.deny(await dssDeploy.owner());
-    await deployer.deploy(GemJoin, await dssDeploy.vat(), web3.utils.asciiToHex('BAT-A'), BAT);
+    await deployer.deploy(GemJoin, MCD_VAT, web3.utils.asciiToHex('BAT-A'), BAT);
     const gemJoin = await GemJoin.deployed();
-    await gemJoin.rely(await pause.proxy());
+    await gemJoin.rely(MCD_PAUSE_PROXY);
     await gemJoin.deny(await dssDeploy.owner());
     await dssDeploy.deployCollateralFlip(web3.utils.asciiToHex('BAT-A'), gemJoin.address, osm.address);
   }
@@ -244,39 +310,59 @@ module.exports = async (deployer, network, [account]) => {
   console.log('Deploying Proxy Actions...');
   await deployer.deploy(DssProxyActions);
   const dssProxyActions = await DssProxyActions.deployed();
+  const PROXY_ACTIONS = dssProxyActions.address;
+  console.log('PROXY_ACTIONS=' + PROXY_ACTIONS);
 
   await deployer.deploy(DssProxyActionsEnd);
   const dssProxyActionsEnd = await DssProxyActionsEnd.deployed();
+  const PROXY_ACTIONS_END = dssProxyActionsEnd.address;
+  console.log('PROXY_ACTIONS_END=' + PROXY_ACTIONS_END);
 
   await deployer.deploy(DssProxyActionsDsr);
   const dssProxyActionsDsr = await DssProxyActionsDsr.deployed();
+  const PROXY_ACTIONS_DSR = dssProxyActionsDsr.address;
+  console.log('PROXY_ACTIONS_DSR=' + PROXY_ACTIONS_DSR);
 
   console.log('Deploying CDP Manager...');
-  await deployer.deploy(DssCdpManager, await dssDeploy.vat());
+  await deployer.deploy(DssCdpManager, MCD_VAT);
   const dssCdpManager = await DssCdpManager.deployed();
+  const CDP_MANAGER = dssCdpManager.address;
+  console.log('CDP_MANAGER=' + CDP_MANAGER);
 
   await deployer.deploy(GetCdps);
   const getCdps = await GetCdps.deployed();
+  const GET_CDPS = getCdps.address;
+  console.log('GET_CDPS=' + GET_CDPS);
 
   console.log('Deploying DSR Manager...');
-  await deployer.deploy(DsrManager, await dssDeploy.pot(), await dssDeploy.daiJoin());
+  await deployer.deploy(DsrManager, MCD_POT, MCD_JOIN_DAI);
   const dsrManager = await DsrManager.deployed();
+  const DSR_MANAGER = dsrManager.address;
+  console.log('DSR_MANAGER=' + DSR_MANAGER);
 
   console.log('Deploying OSM Mom...');
   await deployer.deploy(OsmMom);
   const osmMom = await OsmMom.deployed();
+  const OSM_MOM = osmMom.address;
+  console.log('OSM_MOM=' + OSM_MOM);
 
   console.log('Deploying Flipper Mom...');
-  await deployer.deploy(FlipperMom, await dssDeploy.cat());
+  await deployer.deploy(FlipperMom, MCD_CAT);
   const flipperMom = await FlipperMom.deployed();
+  const FLIPPER_MOM = flipperMom.address;
+  console.log('FLIPPER_MOM=' + FLIPPER_MOM);
 
   console.log('Deploying Clipper Mom...');
-  await deployer.deploy(ClipperMom, await dssDeploy.spotter());
+  await deployer.deploy(ClipperMom, MCD_SPOT);
   const clipperMom = await ClipperMom.deployed();
+  const CLIPPER_MOM = clipperMom.address;
+  console.log('CLIPPER_MOM=' + CLIPPER_MOM);
 
   console.log('Deploying ILK Registry...');
-  await deployer.deploy(IlkRegistry, await dssDeploy.vat(), await dssDeploy.dog(), await dssDeploy.cat(), await dssDeploy.spotter());
+  await deployer.deploy(IlkRegistry, MCD_VAT, MCD_DOG, MCD_CAT, MCD_SPOT);
   const ilkRegistry = await IlkRegistry.deployed();
+  const ILK_REGISTRY = ilkRegistry.address;
+  console.log('ILK_REGISTRY=' + ILK_REGISTRY);
 
   console.log('Releasing Auth...');
   await dssDeploy.releaseAuth();
@@ -290,17 +376,21 @@ module.exports = async (deployer, network, [account]) => {
   console.log('Deploying Gov Actions...');
   await deployer.deploy(GovActions);
   const govActions = await GovActions.deployed();
+  const MCD_GOV_ACTIONS = govActions.address;
+  console.log('MCD_GOV_ACTIONS=' + MCD_GOV_ACTIONS);
 
   console.log('Deploying Pause Proxy Actions...');
   await deployer.deploy(DssDeployPauseProxyActions);
   const dssDeployPauseProxyActions = await DssDeployPauseProxyActions.deployed();
+  const PROXY_PAUSE_ACTIONS = dssDeployPauseProxyActions.address;
+  console.log('PROXY_PAUSE_ACTIONS=' + PROXY_PAUSE_ACTIONS);
 
   console.log('Building Proxy Deployer...');
   await proxyRegistry.build();
   const PROXY_DEPLOYER = await proxyRegistry.proxies(account);
   console.log('PROXY_DEPLOYER=' + PROXY_DEPLOYER);
   const proxyDeployer = await DSProxy.at(PROXY_DEPLOYER);
-  await dsRoles.setRootUser(proxyDeployer.address, true);
+  await dsRoles.setRootUser(PROXY_DEPLOYER, true);
 
   async function rely(who, to) {
     const jsonInterface = {
@@ -313,47 +403,55 @@ module.exports = async (deployer, network, [account]) => {
         { type: 'address', name: 'to' },
       ],
     };
-    return await proxyDeployer.execute(dssDeployPauseProxyActions.address, web3.eth.abi.encodeFunctionCall(jsonInterface, [pause.address, govActions.address, who, to]));
+    const calldata = web3.eth.abi.encodeFunctionCall(jsonInterface, [MCD_PAUSE, MCD_GOV_ACTIONS, who, to]);
+    return await proxyDeployer.methods['execute(address,bytes)'](PROXY_PAUSE_ACTIONS, calldata);
   }
 
   console.log('Publishing IOU Token...');
   await deployer.deploy(DSToken, "IOU");
   const iouToken = await DSToken.deployed();
-  const IOU = iouToken.address;
+  const MCD_IOU = iouToken.address;
+  console.log('MCD_IOU=' + MCD_IOU);
 
   console.log('Publishing DS Chief...');
-  await deployer.deploy(DSChief, GOV, IOU, 5);
+  await deployer.deploy(DSChief, MCD_GOV, MCD_IOU, 5);
   const dsChief = await DSChief.deployed();
-  iouToken.setOwner(dsChief.address);
+  const MCD_ADM_CHIEF = dsChief.address;
+  console.log('MCD_ADM_CHIEF=' + MCD_ADM_CHIEF);
+  iouToken.setOwner(MCD_ADM_CHIEF);
 
   console.log('Publishing Vote Proxy Factory...');
   await deployer.deploy(VoteProxyFactory, dsChief.address);
   const voteProxyFactory = await VoteProxyFactory.deployed();
+  const VOTE_PROXY_FACTORY = voteProxyFactory.address;
+  console.log('VOTE_PROXY_FACTORY=' + VOTE_PROXY_FACTORY);
 
   console.log('Publishing Auto Line...');
-  await deployer.deploy(DssAutoLine, await dssDeploy.vat());
+  await deployer.deploy(DssAutoLine, MCD_VAT);
   const dssAutoLine = await DssAutoLine.deployed();
-  await rely(await dssDeploy.vat(), dssAutoLine.address);
+  const MCD_IAM_AUTO_LINE = dssAutoLine.address;
+  console.log('MCD_IAM_AUTO_LINE=' + MCD_IAM_AUTO_LINE);
+  await rely(MCD_VAT, MCD_IAM_AUTO_LINE);
 
   // PSM
 
   console.log('Deploying AuthGemJoin5...');
-  await deployer.deploy(AuthGemJoin5, await dssDeploy.vat(), web3.utils.asciiToHex('PSM-USDC-A'), vUSDC);
+  await deployer.deploy(AuthGemJoin5, MCD_VAT, web3.utils.asciiToHex('PSM-USDC-A'), vUSDC);
   const authGemJoin5 = await AuthGemJoin5.deployed();
 
   console.log('Deploying DssPsm...');
-  await deployer.deploy(DssPsm, authGemJoin5.address, await dssDeploy.daiJoin(), await dssDeploy.vow());
+  await deployer.deploy(DssPsm, authGemJoin5.address, MCD_JOIN_DAI, MCD_VOW);
   const dssPsm = await DssPsm.deployed();
 
   console.log('Deploying Lerp...');
   await deployer.deploy(Lerp, dssPsm.address, web3.utils.asciiToHex('tin'), LERP_START_TIME, LERP_START, LERP_END, LERP_DURATION);
   const lerp = await Lerp.deployed();
 
-  await authGemJoin5.rely(await pause.proxy());
+  await authGemJoin5.rely(MCD_PAUSE_PROXY);
   await authGemJoin5.rely(dssPsm.address);
   await authGemJoin5.deny(await dssDeploy.owner());
 
-  await dssPsm.rely(await pause.proxy());
+  await dssPsm.rely(MCD_PAUSE_PROXY);
   await dssPsm.rely(lerp.address);
   await dssPsm.deny(await dssDeploy.owner());
 };
