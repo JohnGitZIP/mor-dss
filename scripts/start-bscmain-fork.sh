6a9ba52a1b95a1863458262ce9c0e5b61bd817de
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-08-25 14:26:10 -0300

    Replaces BSC endpoint for Ganache CLI

--- scripts/start-bscmain-fork.sh
+++ scripts/start-bscmain-fork.sh
@@ -4,7 +4,8 @@ source .env
 
 CHAIN_ID=56
 GAS_LIMIT=60000000
-FORK_URL='https://bsc-dataseed.binance.org/'
+FORK_URL='wss://bsc-ws-node.nariox.org:443'
+#FORK_URL='https://bsc-dataseed.binance.org/'
 #FORK_URL='https://apis.ankr.com/'$ANKR_APIKEY_BSCMAIN'/'$ANKR_PROJECT_ID'/binance/full/main'
 
 BALANCE=100000000000000000000000
