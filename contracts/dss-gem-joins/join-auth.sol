71695224249a3f548a5454e536068e652bd95d31
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-08-28 22:21:33 -0300

    Simple fix

--- contracts/dss-gem-joins/join-auth.sol
+++ contracts/dss-gem-joins/join-auth.sol
@@ -52,7 +52,7 @@ contract AuthGemJoin is DSNote {
         live = 0;
     }
 
-    function join(address usr, uint256 wad, address _msgSender) public auth note {
+    function join(address usr, uint256 wad, address _msgSender) public note auth {
         require(live == 1, "AuthGemJoin/not-live");
         require(int256(wad) >= 0, "AuthGemJoin/overflow");
         vat.slip(ilk, usr, int256(wad));
