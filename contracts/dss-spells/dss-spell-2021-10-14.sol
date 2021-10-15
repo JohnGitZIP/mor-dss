5f04d761633c4d6dea62a10fea0fb2d3d359c7f5
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-10-15 13:22:47 -0300

    Reviews test spell

--- contracts/dss-spells/dss-spell-2021-10-14.sol
+++ contracts/dss-spells/dss-spell-2021-10-14.sol
@@ -7,22 +7,22 @@ import { DssExecLib } from "../dss-exec-lib/DssExecLib.sol";
 
 contract DssSpellAction is DssAction
 {
-    // Provides a descriptive tag for bot consumption
-    // This should be modified weekly to provide a summary of the actions
-    // Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/growthdefi/community/master/governance/votes/Executive%20vote%20-%20October%2014%2C%202021.md -q -O - 2>/dev/null)"
-    string public constant override description =
-        "2021-10-14 GrowthDeFi Executive Spell | Hash: 0x0000000000000000000000000000000000000000000000000000000000000000";
+	// Provides a descriptive tag for bot consumption
+	// This should be modified weekly to provide a summary of the actions
+	// Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/GrowthDeFi/community/master/governance/votes/Executive%20vote%20-%20October%2014%2C%202021.md -q -O - 2>/dev/null)"
+	string public constant override description =
+		"2021-10-14 GrowthDeFi Executive Spell | Hash: 0x3fb16482f0c9e06b5c151f11f9d8d81d2623140648a6608de90d190e8358947d";
 
-    function actions() public override
-    {
-        DssExecLib.setChangelogVersion("1.0.0");
-    }
+	function actions() public override
+	{
+		DssExecLib.setChangelogVersion("1.0.0");
+	}
 }
 
 contract DssSpell is DssExec
 {
-    constructor(address log)
-        DssExec(log, block.timestamp + 30 days, address(new DssSpellAction())) public
-    {
-    }
+	constructor(address log)
+		DssExec(log, block.timestamp + 30 days, address(new DssSpellAction())) public
+	{
+	}
 }
