df6e298b428630c955eed1cba2fb72a94665260f
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-11-06 19:38:59 -0300

    Adds support for Avalanche to pokebot

--- scripts/pokebot.js
+++ scripts/pokebot.js
@@ -65,14 +65,17 @@ const privateKey = process.env['PRIVATE_KEY'] || '';
 
 const ADDRESS_URL_PREFIX = {
   'bscmain': 'https://bscscan.com/address/',
+  'avaxmain': 'https://snowtrace.io/address/',
 };
 
 const TX_URL_PREFIX = {
   'bscmain': 'https://bscscan.com/tx/',
+  'avaxmain': 'https://snowtrace.io/tx/',
 };
 
 const NATIVE_SYMBOL = {
   'bscmain': 'BNB',
+  'bscmain': 'AVAX',
 };
 
 const HTTP_PROVIDER_URLS = {
@@ -90,6 +93,9 @@ const HTTP_PROVIDER_URLS = {
     'https://bsc-dataseed2.binance.org/',
     'https://bsc-dataseed3.binance.org/',
     'https://bsc-dataseed4.binance.org/',
+  ],
+  'avaxmain': [
+    'https://api.avax.network/ext/bc/C/rpc',
   ],
 };
 
@@ -147,56 +153,127 @@ const PIPLIKE_ABI = require('../build/contracts/PipLike.json').abi;
 const UNIV2LPORACLE_ABI = require('../build/contracts/UNIV2LPOracle.json').abi;
 
 const PIP_LIST = {
-  'PSM_BUSD': { address: '0x4C4119f8438CC66CE21414dC7d09437954433C78', type: 'value' },
-  'BUSD':     { address: '0x08F39c96E6A954894252171a5300dECD350d3fA8', type: 'chainlink' },
-  'USDC':     {  address: '0xd4d7BCF6c7b54349C91f39cAd89B228C53FE6BD7', type: 'chainlink' },
-  'BNB':       { address: '0x63c2E42758EF8776BF7b70afb00E0e2748Ad3F05', type: 'chainlink' },
-  'ETH':       { address: '0x7622ce6588116c1C7F1a4E61A153C1efC7226f78', type: 'chainlink' },
-  'BTCB':     { address: '0x585707c57413e09a4BE58e89798f5074b2B89De1', type: 'chainlink' },
-  'CAKE':     { address: '0x447FE0cc2145F27127Cf60C6FD6D9025A4208b8B', type: 'chainlink' },
-  'BANANA':    { address: '0x6Ee2E2d648698357Cc518D1D5E8170586dca5348', type: 'twap' },
-  'MOR':       { address: '0x3Ac5DF5d1a97E66d9a20c90961daaBcf9EC34B06', type: 'twap' },
+  'bscmain': {
+    'PSM_BUSD': { address: '0x4C4119f8438CC66CE21414dC7d09437954433C78', type: 'value' },
 
-  'PCSBNBCAKE': { address: '0x326Db2b9640e51077fD9B70767855f5c2128e91A', type: 'univ2lp' },
-  'PCSBNBBUSD': { address: '0x1a06452B84456728Ee4054AE6157d3feDF56C295', type: 'univ2lp' },
-  'PCSBNBETH': { address: '0x8BBcd7E4da4395E391Fbfc2A11775debe3ca0D58', type: 'univ2lp' },
-  'PCSBNBBTCB': { address: '0xcf55226EE56F174B3cB3F75a5182d2300e788e91', type: 'univ2lp' },
-  'PCSBUSDUSDC': { address: '0xC5065b47A133071fe8cD94f46950fCfBA53864C6', type: 'univ2lp' },
-  'PCSBUSDBTCB': { address: '0x3d4604395595Bb30A8B7754b5dDBF0B3F680564b', type: 'univ2lp' },
-  'PCSBUSDCAKE': { address: '0x1e1ee1AcD4B7ad405A0D701884F093d54DF7fba4', type: 'univ2lp' },
-  'PCSETHBTCB': { address: '0x58849cE72b4E4338C00f0760Ca6AfCe11b5ee370', type: 'univ2lp' },
-  'PCSETHUSDC': { address: '0xc690F38430Db2057C992c3d3190D9902CD7E0294', type: 'univ2lp' },
-  'APEMORBUSD': { address: '0x2987bC4DD60A0bC8801ADCE4EdFB1efB6781A984', type: 'univ2lp' },
+    'BUSD': { address: '0x08F39c96E6A954894252171a5300dECD350d3fA8', type: 'chainlink' },
+    'USDC': {  address: '0xd4d7BCF6c7b54349C91f39cAd89B228C53FE6BD7', type: 'chainlink' },
+    'BNB': { address: '0x63c2E42758EF8776BF7b70afb00E0e2748Ad3F05', type: 'chainlink' },
+    'ETH': { address: '0x7622ce6588116c1C7F1a4E61A153C1efC7226f78', type: 'chainlink' },
+    'BTCB': { address: '0x585707c57413e09a4BE58e89798f5074b2B89De1', type: 'chainlink' },
+    'CAKE': { address: '0x447FE0cc2145F27127Cf60C6FD6D9025A4208b8B', type: 'chainlink' },
+    'BANANA': { address: '0x6Ee2E2d648698357Cc518D1D5E8170586dca5348', type: 'twap' },
+    'MOR': { address: '0x3Ac5DF5d1a97E66d9a20c90961daaBcf9EC34B06', type: 'twap' },
 
-  'STKCAKE': { address: '0xeE991787C4ffE1de8c8c7c45e3EF14bFc47A2735', type: 'vault' },
-  'STKBANANA': { address: '0xE4d5a6E0581646f5a5806F9c171E96879ae8b385', type: 'vault' },
+    'PCSBNBCAKE': { address: '0x326Db2b9640e51077fD9B70767855f5c2128e91A', type: 'univ2lp' },
+    'PCSBNBBUSD': { address: '0x1a06452B84456728Ee4054AE6157d3feDF56C295', type: 'univ2lp' },
+    'PCSBNBETH': { address: '0x8BBcd7E4da4395E391Fbfc2A11775debe3ca0D58', type: 'univ2lp' },
+    'PCSBNBBTCB': { address: '0xcf55226EE56F174B3cB3F75a5182d2300e788e91', type: 'univ2lp' },
+    'PCSBUSDUSDC': { address: '0xC5065b47A133071fe8cD94f46950fCfBA53864C6', type: 'univ2lp' },
+    'PCSBUSDBTCB': { address: '0x3d4604395595Bb30A8B7754b5dDBF0B3F680564b', type: 'univ2lp' },
+    'PCSBUSDCAKE': { address: '0x1e1ee1AcD4B7ad405A0D701884F093d54DF7fba4', type: 'univ2lp' },
+    'PCSETHBTCB': { address: '0x58849cE72b4E4338C00f0760Ca6AfCe11b5ee370', type: 'univ2lp' },
+    'PCSETHUSDC': { address: '0xc690F38430Db2057C992c3d3190D9902CD7E0294', type: 'univ2lp' },
+    'APEMORBUSD': { address: '0x2987bC4DD60A0bC8801ADCE4EdFB1efB6781A984', type: 'univ2lp' },
 
-  'STKPCSBNBCAKE': { address: '0x5Df1B3212EB26f506af448cE25cd4E315BEdf630', type: 'vault' },
-  'STKPCSBNBBUSD': { address: '0x8a8eA20937BBC38c0952b206892e9A273E7180E1', type: 'vault' },
-  'STKPCSBNBETH': { address: '0x0Ca167778392473E0868503522a11f1e749bbF82', type: 'vault' },
-  'STKPCSBNBBTCB': { address: '0x7e7C92D432307218b94052488B2CD54D8b826546', type: 'vault' },
-  'STKPCSBUSDUSDC': { address: '0x7bA715959A52ef046BE76c4E32f1de1d161E2888', type: 'vault' },
-  'STKPCSBUSDBTCB': { address: '0x8652883985B39D85B6432e3Ec5D9bea77edc31b0', type: 'vault' },
-  'STKPCSBUSDCAKE': { address: '0xeBcb52E5696A2a90D684C76cDf7095534F265370', type: 'vault' },
-  'STKPCSETHBTCB': { address: '0x70AF6F516f9E167620a5bdd970c671c69C81E92F', type: 'vault' },
-  'STKPCSETHUSDC': { address: '0x68697fF7Ec17F528E3E4862A1dbE6d7D9cBBd5C6', type: 'vault' },
-  'STKAPEMORBUSD': { address: '0x627A13421df5Ff3FdF8f56AF2911c287ad8CbE9f', type: 'vault' },
+    'STKCAKE': { address: '0xeE991787C4ffE1de8c8c7c45e3EF14bFc47A2735', type: 'vault' },
+    'STKBANANA': { address: '0xE4d5a6E0581646f5a5806F9c171E96879ae8b385', type: 'vault' },
+
+    'STKPCSBNBCAKE': { address: '0x5Df1B3212EB26f506af448cE25cd4E315BEdf630', type: 'vault' },
+    'STKPCSBNBBUSD': { address: '0x8a8eA20937BBC38c0952b206892e9A273E7180E1', type: 'vault' },
+    'STKPCSBNBETH': { address: '0x0Ca167778392473E0868503522a11f1e749bbF82', type: 'vault' },
+    'STKPCSBNBBTCB': { address: '0x7e7C92D432307218b94052488B2CD54D8b826546', type: 'vault' },
+    'STKPCSBUSDUSDC': { address: '0x7bA715959A52ef046BE76c4E32f1de1d161E2888', type: 'vault' },
+    'STKPCSBUSDBTCB': { address: '0x8652883985B39D85B6432e3Ec5D9bea77edc31b0', type: 'vault' },
+    'STKPCSBUSDCAKE': { address: '0xeBcb52E5696A2a90D684C76cDf7095534F265370', type: 'vault' },
+    'STKPCSETHBTCB': { address: '0x70AF6F516f9E167620a5bdd970c671c69C81E92F', type: 'vault' },
+    'STKPCSETHUSDC': { address: '0x68697fF7Ec17F528E3E4862A1dbE6d7D9cBBd5C6', type: 'vault' },
+    'STKAPEMORBUSD': { address: '0x627A13421df5Ff3FdF8f56AF2911c287ad8CbE9f', type: 'vault' },
+  },
+
+  'avaxmain': {
+    'PSM_STKUSDC': { address: '0x68697fF7Ec17F528E3E4862A1dbE6d7D9cBBd5C6', type: 'value' },
+
+    'AVAX': { address: '0xd4d7BCF6c7b54349C91f39cAd89B228C53FE6BD7', type: 'chainlink' },
+    'WETH': { address: '0x63c2E42758EF8776BF7b70afb00E0e2748Ad3F05', type: 'chainlink' },
+    'WBTC': { address: '0x7622ce6588116c1C7F1a4E61A153C1efC7226f78', type: 'chainlink' },
+    'DAI': { address: '0x585707c57413e09a4BE58e89798f5074b2B89De1', type: 'chainlink' },
+    'USDC': { address: '0x447FE0cc2145F27127Cf60C6FD6D9025A4208b8B', type: 'chainlink' },
+    'USDT': { address: '0x6Ee2E2d648698357Cc518D1D5E8170586dca5348', type: 'chainlink' },
+    'LINK': { address: '0x326Db2b9640e51077fD9B70767855f5c2128e91A', type: 'chainlink' },
+    'MIM': { address: '0x1B87083Af792cB8355C4c954c491255482992E79', type: 'chainlink' },
+    'JOE': { address: '0x1a06452B84456728Ee4054AE6157d3feDF56C295', type: 'twap' },
+
+    'XJOE': { address: '0xF49390eE384C5df2e82ac99909a6236051a4E82B', type: 'xsushi' },
+    'JAVAX': { address: '0x8BBcd7E4da4395E391Fbfc2A11775debe3ca0D58', type: 'comp' },
+    'JWETH': { address: '0xAB47baC3C131eD3ac9d8F993fD2D902cad460c0f', type: 'comp' },
+    'JWBTC': { address: '0xcf55226EE56F174B3cB3F75a5182d2300e788e91', type: 'comp' },
+    'JLINK': { address: '0xB31fF116f5fEC1C0Aee2Aa86d5E78e3105CC4274', type: 'comp' },
+
+    'TDJAVAXJOE': { address: '0xC5065b47A133071fe8cD94f46950fCfBA53864C6', type: 'univ2lp' },
+    'TDJAVAXWETH': { address: '0x3d4604395595Bb30A8B7754b5dDBF0B3F680564b', type: 'univ2lp' },
+    'TDJAVAXWBTC': { address: '0x1e1ee1AcD4B7ad405A0D701884F093d54DF7fba4', type: 'univ2lp' },
+    'TDJAVAXDAI': { address: '0x58849cE72b4E4338C00f0760Ca6AfCe11b5ee370', type: 'univ2lp' },
+    'TDJAVAXUSDC': { address: '0xc690F38430Db2057C992c3d3190D9902CD7E0294', type: 'univ2lp' },
+    'TDJAVAXUSDT': { address: '0xeE991787C4ffE1de8c8c7c45e3EF14bFc47A2735', type: 'univ2lp' },
+    'TDJAVAXLINK': { address: '0x5Df1B3212EB26f506af448cE25cd4E315BEdf630', type: 'univ2lp' },
+    'TDJAVAXMIM': { address: '0x0Ca167778392473E0868503522a11f1e749bbF82', type: 'univ2lp' },
+    'TDJUSDCJOE': { address: '0x7bA715959A52ef046BE76c4E32f1de1d161E2888', type: 'univ2lp' },
+    'TDJUSDTJOE': { address: '0xeBcb52E5696A2a90D684C76cDf7095534F265370', type: 'univ2lp' },
+
+    'STKXJOE': { address: '0xf72f07b96D4Ee64d1065951cAfac032B63C767bb', type: 'vault' },
+    'STKJAVAX': { address: '0xeeF286Af1d7601EA5E40473741D79e55770498d8', type: 'vault' },
+    'STKJWETH': { address: '0xa9b68E3E65966B1C08cfa6002E8527E091e5664e', type: 'vault' },
+    'STKJWBTC': { address: '0x5ef900FD5aACd6CFe994b2E13c3d4aBDD9fFea2b', type: 'vault' },
+    'STKJLINK': { address: '0x3728Bd61F582dA0b22cFe7EDC59aC33f7402c4e0', type: 'vault' },
+
+    'STKTDJAVAXJOE': { address: '0xca70528209917F4D0443Dd3e90C863b19584CCAF', type: 'vault' },
+    'STKTDJAVAXWETH': { address: '0x352C748Ff550Eec6355e37Ee62459210909709DD', type: 'vault' },
+    'STKTDJAVAXWBTC': { address: '0x260e6061233A3F05213a54103A9F0460857f9E9c', type: 'vault' },
+    'STKTDJAVAXDAI': { address: '0x9605863bf02E983861C0a4ac28a7527Fcf36732b', type: 'vault' },
+    'STKTDJAVAXUSDC': { address: '0x2117C852417B008d18E292D18ab196f49AA896cf', type: 'vault' },
+    'STKTDJAVAXUSDT': { address: '0x6B61e028199BCC4760fD9CC5DEfC7430d977FC08', type: 'vault' },
+    'STKTDJAVAXLINK': { address: '0x4A1dB63A8240A030C7E8678c594711D139a1c39f', type: 'vault' },
+    'STKTDJAVAXMIM': { address: '0x842B07b7D9C77A6bE833a660FB628C6d28Bda0a8', type: 'vault' },
+    'STKTDJUSDCJOE': { address: '0x7253bC2Ca443807391451a54cAF1bC1915A8b584', type: 'vault' },
+    'STKTDJUSDTJOE': { address: '0xed219cD2aF00625e0c1aD21b7cC7aa0f77601860', type: 'vault' },
+  },
 };
 
-const ILK_LIST = [
-  'STKCAKE-A',
-  'STKBANANA-A',
-  'STKPCSBNBCAKE-A',
-  'STKPCSBNBBUSD-A',
-  'STKPCSBNBETH-A',
-  'STKPCSBNBBTCB-A',
-  'STKPCSBUSDUSDC-A',
-  'STKPCSBUSDBTCB-A',
-  'STKPCSBUSDCAKE-A',
-  'STKPCSETHBTCB-A',
-  'STKPCSETHUSDC-A',
-  'STKAPEMORBUSD-A',
-];
+const ILK_LIST = {
+  'bscmain': [
+    'STKCAKE-A',
+    'STKBANANA-A',
+    'STKPCSBNBCAKE-A',
+    'STKPCSBNBBUSD-A',
+    'STKPCSBNBETH-A',
+    'STKPCSBNBBTCB-A',
+    'STKPCSBUSDUSDC-A',
+    'STKPCSBUSDBTCB-A',
+    'STKPCSBUSDCAKE-A',
+    'STKPCSETHBTCB-A',
+    'STKPCSETHUSDC-A',
+    'STKAPEMORBUSD-A',
+  ],
+
+  'avaxmain': [
+    'STKXJOE-A',
+    'STKJAVAX-A',
+    'STKJWETH-A',
+    'STKJWBTC-A',
+    'STKJLINK-A',
+    'STKTDJAVAXJOE-A',
+    'STKTDJAVAXWETH-A',
+    'STKTDJAVAXWBTC-A',
+    'STKTDJAVAXDAI-A',
+    'STKTDJAVAXUSDC-A',
+    'STKTDJAVAXUSDT-A',
+    'STKTDJAVAXLINK-A',
+    'STKTDJAVAXMIM-A',
+    'STKTDJUSDCJOE-A',
+    'STKTDJUSDTJOE-A',
+  ],
+};
 
 function getDefaultAccount(privateKey, network) {
   const web3 = getWeb3(privateKey, network);
@@ -357,8 +434,8 @@ async function pokeAll(network, lines = []) {
   }
 
   // poke PIPs
-  for (const name in PIP_LIST) {
-    const { address, type } = PIP_LIST[name];
+  for (const name in PIP_LIST[network]) {
+    const { address, type } = PIP_LIST[network][name];
     const data = { name, address, type };
     if (type === 'univ2lp') {
       const now = Math.floor(Date.now() / 1000);
@@ -376,6 +453,15 @@ async function pokeAll(network, lines = []) {
         }
         if (name === 'APEMORBUSD') {
           const name = 'MOR';
+          const address = pips[name].address;
+          const nonce = await getNonce(privateKey, network);
+          console.log('Poking ' + name + ' at nonce ' + nonce + '...');
+          const tx = await poke(privateKey, network, address, nonce);
+          do { await sleep(3 * 1000); } while (await getNonce(privateKey, network) <= nonce);
+          log(name, 'twap', address, tx);
+        }
+        if (name === 'TDJUSDTJOE') {
+          const name = 'JOE';
           const address = pips[name].address;
           const nonce = await getNonce(privateKey, network);
           console.log('Poking ' + name + ' at nonce ' + nonce + '...');
@@ -405,7 +491,7 @@ async function pokeAll(network, lines = []) {
   }
 
   // poke spotter ILKs
-  for (const name of ILK_LIST) {
+  for (const name of ILK_LIST[network]) {
     const [token, ilk] = name.split('-');
     const { value } = pips[token];
     if (value !== undefined) {
